3c3cebd61eeed66ae0df48a124fac2248d7681c2
luuuuyi1107 <luuuyi1107@gmail.com>
2025-06-30 02:04:58 +0800

    update

--- assets/js/common.js
+++ assets/js/common.js
@@ -13,42 +13,17 @@ document.getElementById('end_date').addEventListener('click', function() {
 });
 
 // 展開/收起手機子選單
-// function turnMobileMenu(number, subNumber){
-//   if (!subNumber) {
-//     if ($('#no_' + number + '_btn').hasClass('menu_level_1_active')) {
-//       $('.mobile_operate_level_1').removeClass('menu_level_1_active')
-//       $('.mobile_sub_menu_list_1').css("display",'none')
-//     } else {
-//       $('.mobile_operate_level_1').removeClass('menu_level_1_active')
-//       $('.mobile_sub_menu_list_1').css("display",'none')
-//       $('#no_' + number + '_btn').addClass('menu_level_1_active')
-//       $('#no_' + number + '_menu').css("display",'block')
-//     }
-//   } else {
-//     if ($('#no_' + number + '_sub_' + subNumber + '_btn').hasClass('menu_level_2_active')) {
-//       $('.mobile_operate_level_2').removeClass('menu_level_2_active')
-//       $('.mobile_sub_menu_list_2').css("display",'none')
-//     } else {
-//       $('.mobile_operate_level_2').removeClass('menu_level_2_active')
-//       $('.mobile_sub_menu_list_2').css("display",'none')
-//       $('#no_' + number + '_sub_' + subNumber + '_btn').addClass('menu_level_2_active')
-//       $('#no_' + number + '_sub_' + subNumber + '_menu').css("display",'block')
-//     }
-//   }
-// }
+function turnMobileMenu(){
+    const mobileMenu = document.getElementById('mobile_menu');
+    if (mobileMenu.classList.contains('active')) {
+      mobileMenu.classList.remove('active');
+    } else {
+      mobileMenu.classList.add('active');
+    }
+}
 
-// $(window).resize((e) => {
-//   if ( window.innerWidth > 1200) {
-//     this.menuTurn('close')
-//   }
-// })
-
-
-// $(window).scroll((e) => {
-//   // 返回頂部按鈕顯示判斷
-//   if ($(document).scrollTop() > 500) {
-//     $('.top_btn').css("display",'flex')
-//   }else{
-//     $('.top_btn').css("display",'none')
-//   }
-// })
+$(window).resize((e) => {
+  if ( window.innerWidth > 1000) {
+    document.getElementById('mobile_menu').classList.remove('active');
+  }
+})
